b29a4577857e8f6f38d52f0ef2f325acc884353a
Edi Wang <Edi.Wang@outlook.com>
2021-03-01 13:49:45 +0800

    basic functionalituy

--- src/Pages/Index.razor.cs
+++ src/Pages/Index.razor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Calc24Blazor.Pages
@@ -12,5 +13,139 @@ namespace Calc24Blazor.Pages
         public int Num3 { get; set; }
         public int Num4 { get; set; }
 
+        public List<string> ResultList { get; set; } = new();
+
+        private void GetResults()
+        {
+            ResultList.Clear();
+
+            var list = DoCalc();
+            ResultList = list;
+        }
+
+        private List<string> DoCalc()
+        {
+            var tempList = new List<string>();
+
+            List<double> numbers = new List<double> { Num1, Num2, Num3, Num4 };
+
+            var operators = new List<Func<Expression, Expression, BinaryExpression>>
+            {
+                Expression.Add,
+                Expression.Subtract,
+                Expression.Multiply,
+                Expression.Divide
+            };
+
+            foreach (var operatorCombination in OperatorPermute(operators))
+            {
+                foreach (Node node in AllBinaryTrees(3))
+                {
+                    foreach (List<double> permuteOfNumbers in FullPermute(numbers))
+                    {
+                        Expression expression = Build(node, permuteOfNumbers, operatorCombination.ToList());
+                        Func<double> compiled = Expression.Lambda<Func<double>>(expression).Compile();
+                        try
+                        {
+                            var value = compiled();
+                            if (Math.Abs(value - 24) < 0.01)
+                            {
+                                tempList.Add($"{expression} = {value}");
+                            }
+                        }
+                        catch (DivideByZeroException)
+                        {
+                        }
+                    }
+                }
+            }
+
+            return tempList;
+        }
+
+        private static IEnumerable<IEnumerable<Func<Expression, Expression, BinaryExpression>>> OperatorPermute(List<Func<Expression, Expression, BinaryExpression>> operators)
+        {
+            return from operator1 in operators
+                   from operator2 in operators
+                   from operator3 in operators
+                   select new[] { operator1, operator2, operator3 };
+        }
+
+        private static IEnumerable<List<T>> FullPermute<T>(List<T> elements)
+        {
+            if (elements.Count == 1)
+                return EnumerableOfOneElement(elements);
+
+            IEnumerable<List<T>> result = null;
+            foreach (T first in elements)
+            {
+                List<T> remaining = elements.ToArray().ToList();
+                remaining.Remove(first);
+                IEnumerable<List<T>> fullPermuteOfRemaining = FullPermute(remaining);
+
+                foreach (List<T> permute in fullPermuteOfRemaining)
+                {
+                    var arr = new List<T> { first };
+                    arr.AddRange(permute);
+
+                    var seq = EnumerableOfOneElement(arr);
+                    result = result?.Union(seq) ?? seq;
+                }
+            }
+            return result;
+        }
+
+        private static IEnumerable<T> EnumerableOfOneElement<T>(T element)
+        {
+            yield return element;
+        }
+
+        private static Expression Build(Node node, List<double> numbers, List<Func<Expression, Expression, BinaryExpression>> operators)
+        {
+            var iNum = 0;
+            var iOprt = 0;
+
+            Func<Node, Expression> f = null;
+            f = n =>
+            {
+                Expression exp;
+                if (n == null)
+                {
+                    exp = Expression.Constant(numbers[iNum++]);
+                }
+                else
+                {
+                    var left = f(n.Left);
+                    var right = f(n.Right);
+                    exp = operators[iOprt++](left, right);
+                }
+                return exp;
+            };
+            return f(node);
+        }
+
+        private static IEnumerable<Node> AllBinaryTrees(int size)
+        {
+            if (size == 0)
+            {
+                return new Node[] { null };
+            }
+
+            return from i in Enumerable.Range(0, size)
+                   from left in AllBinaryTrees(i)
+                   from right in AllBinaryTrees(size - 1 - i)
+                   select new Node(left, right);
+        }
+    }
+
+    internal class Node
+    {
+        public Node Left { get; private set; }
+        public Node Right { get; private set; }
+        public Node(Node left, Node right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
     }
 }
